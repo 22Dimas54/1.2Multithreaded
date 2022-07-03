3d2b80d69b8f45c3b3fbd8081f0b745039dd4ac1
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-07-03 08:49:23 +0700

    task 2

--- src/ru/netology/honeybadger/Manufacturer.java
+++ src/ru/netology/honeybadger/Manufacturer.java
@@ -1,37 +1,49 @@
 package ru.netology.honeybadger;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Manufacturer {
     private final int AUTO_ASSEMBLY_TIME = 3000;
     private final int TIME_BUYING_CAR = 3000;
     private CarDealership carDealership;
+    private ReentrantLock reentrantLock = new ReentrantLock(true);
+    private Condition condition = reentrantLock.newCondition();
+
 
     public Manufacturer(CarDealership carDealership) {
         this.carDealership = carDealership;
     }
 
-    public synchronized void releaseCar() {
+    public void releaseCar() {
         try {
+            reentrantLock.lock();
             System.out.printf("Производитель  %s собирает авто!\n", Thread.currentThread().getName());
             Thread.sleep(AUTO_ASSEMBLY_TIME);
             carDealership.getCars().add(new Car());
             System.out.printf("Производитель  %s выпустил 1 авто!\n", Thread.currentThread().getName());
-            notify();
+            condition.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
         }
     }
 
-    public synchronized void sellCar() {
+    public void sellCar() {
         try {
+            reentrantLock.lock();
             System.out.printf("%s: зашел в автосалон!\n", Thread.currentThread().getName());
             while (carDealership.getCars().size() == 0) {
                 System.out.printf("%s: Машин нет\n", Thread.currentThread().getName());
-                wait();
+                condition.await();
             }
             Thread.sleep(TIME_BUYING_CAR);
             System.out.printf("%s: уехал на новеньком авто!\n", Thread.currentThread().getName());
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
         }
     }
 }
