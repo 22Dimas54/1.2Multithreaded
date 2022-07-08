93e174fa252a178afe5b7ab44ee07b72300659a4
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-07-08 09:28:08 +0700

    fix logic

--- src/ru/netology/honeybadger/CarDealership.java
+++ src/ru/netology/honeybadger/CarDealership.java
@@ -4,18 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarDealership {
-    final Manufacturer manufacturer = new Manufacturer(this);
+    //final Manufacturer manufacturer = new Manufacturer(this);
+    private static final int TIME_BUYING_CAR = 2000;
     List<Car> cars = new ArrayList<>();
 
     public List<Car> getCars() {
         return cars;
     }
 
-    public void sellCar() {
-        manufacturer.sellCar();
+    public synchronized void sellCar() {
+        try {
+            Thread.currentThread().setName("Покупатель " + (int) (Math.random() * TIME_BUYING_CAR));
+            System.out.printf("%s: зашел в автосалон!\n", Thread.currentThread().getName());
+            while (this.cars.size() == 0) {
+                System.out.printf("%s: Машин нет\n", Thread.currentThread().getName());
+                wait();
+            }
+            Thread.sleep(TIME_BUYING_CAR);
+            System.out.printf("%s: уехал на новеньком авто!\n", Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void releaseCar() {
-        manufacturer.releaseCar();
+    public synchronized void movingInCarDealership() {
+        notify();
     }
 }
--- src/ru/netology/honeybadger/Manufacturer.java
+++ src/ru/netology/honeybadger/Manufacturer.java
@@ -2,34 +2,19 @@ package ru.netology.honeybadger;
 
 public class Manufacturer {
     private static final int AUTO_ASSEMBLY_TIME = 3000;
-    private static final int TIME_BUYING_CAR = 3000;
     private CarDealership carDealership;
 
     public Manufacturer(CarDealership carDealership) {
         this.carDealership = carDealership;
     }
 
-    public synchronized void releaseCar() {
+    public void releaseCar() {
         try {
-            System.out.printf("Производитель  %s собирает авто!\n", Thread.currentThread().getName());
+            System.out.println("Производитель  BMW собирает авто!");
             Thread.sleep(AUTO_ASSEMBLY_TIME);
             carDealership.getCars().add(new Car());
-            System.out.printf("Производитель  %s выпустил 1 авто!\n", Thread.currentThread().getName());
-            notify();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public synchronized void sellCar() {
-        try {
-            System.out.printf("%s: зашел в автосалон!\n", Thread.currentThread().getName());
-            while (carDealership.getCars().size() == 0) {
-                System.out.printf("%s: Машин нет\n", Thread.currentThread().getName());
-                wait();
-            }
-            Thread.sleep(TIME_BUYING_CAR);
-            System.out.printf("%s: уехал на новеньком авто!\n", Thread.currentThread().getName());
+            System.out.println("Производитель  BMW выпустил 1 авто!");
+            carDealership.movingInCarDealership();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
